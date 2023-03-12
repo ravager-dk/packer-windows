c97d55d9ffebed2e1cbbb470ff29c577630903cc
Stefan Scherer <scherer_stefan@icloud.com>
2023-03-12 10:36:59 +0100

    Update Windows Server 2022 ISO URL

--- build_windows_2022_docker.ps1
+++ build_windows_2022_docker.ps1
@@ -4,5 +4,5 @@ if (Test-Path ./output-hyperv-iso) {
 
 packer build --only=hyperv-iso `
   --var iso_url=./iso/20348.169.210806-2348.fe_release_svc_refresh_SERVER_EVAL_x64FRE_en-us.iso `
-  --var iso_checksum="sha256:4f1457c4fe14ce48c9b2324924f33ca4f0470475e6da851b39ccbf98f44e7852" `
+  --var iso_checksum="sha256:3e4fa6d8507b554856fc9ca6079cc402df11a8b79344871669f0251535255325" `
   windows_2022_docker.json
--- build_windows_2022_docker.sh
+++ build_windows_2022_docker.sh
@@ -5,5 +5,5 @@ packer build \
   --var vhv_enable=true \
   --var autounattend=./tmp/2022_core/Autounattend.xml \
   --var iso_url=~/.cache/packer/caff720c675a0670657cb6bcfb438d3b0c664081.iso \
-  --var iso_checksum="sha256:4f1457c4fe14ce48c9b2324924f33ca4f0470475e6da851b39ccbf98f44e7852" \
+  --var iso_checksum="sha256:3e4fa6d8507b554856fc9ca6079cc402df11a8b79344871669f0251535255325" \
   windows_2022_docker.json
